02befb4314fc220e34108686961350462f2c7659
EstelleDy <36534909+EstelleDy@users.noreply.github.com>
2019-05-25 21:41:23 +0200

    Add files via upload

--- DetailBulletin.java
+++ DetailBulletin.java
@@ -1,41 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package modele;
+package Controleur;
 
-/**
- *
- * @author Isabelle
- */
 public class DetailBulletin {
-   private int id_detailBulletin;
-   private String appreciation_b;
-  
-   public DetailBulletin(){
-     this.id_detailBulletin=0; 
-     this.appreciation_b=null;
-   } 
-    public DetailBulletin(int id_detailBulletin, String appreciation_b){
-     this.id_detailBulletin=id_detailBulletin; 
-     this.appreciation_b=appreciation_b;
-   }
-   public void setId_detailBulletin(int id_bulletin){
-   this.id_detailBulletin=id_detailBulletin;
-   } 
-    public void setAppreciationB(String appreciation_b){
-   this.appreciation_b=appreciation_b;
-   }
-   public int getId_detailBulletin(){
-       return this.id_detailBulletin;
-   } 
-   public String getAppreciationB(){
-       return this.appreciation_b;
-   }
-   public void display(){
-     System.out.println("L'id du bulletin est:"+id_detailBulletin);
-     System.out.println("L' appréciation générale est :"+appreciation_b);
-   }    
+
+
+    private:
+
+    int id_detailBulletin;
+    String appreciation_b;
 }
-   
--- Trimestre.java
+++ Trimestre.java
@@ -1,58 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package modele;
+package Controleur;
 
-/**
- *
- * @author Isabelle
- */
 public class Trimestre {
-   private int id_trimestre;
-   private int numero;
-   private int debut;
-   private int fin;
-   public Trimestre(){
-     this.id_trimestre=0;
-     this.numero=0;
-     this.debut=0;
-     this.fin=0;
-   } 
-    public Trimestre(int id_trimestre, int numero, int debut, int fin){
-     this.id_trimestre=id_trimestre;
-     this.numero=numero;
-     this.debut=debut;
-     this.fin=fin;
-   }
-   public void setId(int id){
-   this.id_trimestre=id;
-   }
-   public void setNumero(int numero){
-   this.numero=numero;
-   }
-    public void setDebut(int debut){
-   this.debut=debut;
-   }
-     public void setFin(int fin){
-   this.fin=fin;
-   }
-   public int getId(){
-     return this.id_trimestre;  
-   }   
-   public int getNumero(){
-     return this.numero;  
-   }
-    public int getDebut(){
-     return this.debut;  
-   } 
-   public int getFin(){
-     return this.fin;  
-   }  
-   public void display(){
-    System.out.println("L'id du trimestre est :"+id_trimestre);
-    System.out.println("Nous sommes le "+numero+ "e du trimestre");
-    System.out.println("Le debut est "+debut+ " la fin est "+fin);
-   }   
+
+    private:
+    
+    int id_trimestre;
+    int numero;
+    int debut;
+    int fin;
 }
